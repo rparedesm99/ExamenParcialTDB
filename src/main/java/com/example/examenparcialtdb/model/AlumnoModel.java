e86330647a7afb07886094747dec8e9a4168ebd1
rparedes99 <romerparedesm@gmail.com>
2024-11-19 14:24:06 -0500

    debug

--- src/main/java/com/example/examenparcialtdb/model/AlumnoModel.java
+++ src/main/java/com/example/examenparcialtdb/model/AlumnoModel.java
@@ -1,6 +1,9 @@
 package com.example.examenparcialtdb.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class AlumnoModel {
